60bab759b897b9fd8ad2137fea9445f5b18c5cb0
Jere Helenius <Jere.helenius2@gmail.com>
2025-09-16 10:28:18 +0300

    error updates

--- index.js
+++ index.js
@@ -16,18 +16,6 @@ app.use(express.json())
 morgan.token("body", request => JSON.stringify(request.body))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
-const errorhandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" })
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message })
-  }
-  next(error)
-}
-app.use(errorhandler)
-
 app.get("/info", (request, response) => {
   Person.countDocuments({})
     .then(count => 
@@ -97,5 +85,16 @@ app.put("/api/persons/:id", (request, response, next) => {
     .catch(error => next(error))
 })
 
+const errorhandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorhandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- module/person.js
+++ module/person.js
@@ -5,14 +5,28 @@ const personsSchema = new mongoose.Schema({
     type: String,
     minlength: 3,
     required: true,
-    unique: true
   },
   number: {
     type: String,
     minlength: 8,
-    required: true
+    required: true,
+    validate: {
+      validator: function(v) {
+        const parts = v.split('-')
+        if (parts.length !== 2) return false
+
+        const [first, second] = parts;
+
+        
+        if (!/^\d{2,3}$/.test(first)) return false;
+        if (!/^\d{2,}$/.test(second)) return false;
+        
+
+        return true;
+    },      message: props => `${props.value} is not a valid phone number!`
+    }
   }
-})
+});
 
 personsSchema.set('toJSON', {
   transform: (document, returnedObject) => {
